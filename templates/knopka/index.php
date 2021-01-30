78c86b5977282f44466ae3a7875694311936163e
Klyuchko Andrey <andreyalek@gmail.com>
2021-01-30 17:33:15 +0200

    Added google analytics

--- templates/knopka/index.php
+++ templates/knopka/index.php
@@ -169,6 +169,18 @@ if ($this->params->get('logoFile')) {
     <link href="/templates/knopka/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
     <link href="/templates/knopka/css/template.css?4ee1df3092c1fe2c58d44d2646a0a0b6" rel="stylesheet">
     <jdoc:include type="head" />
+
+    <? if($_SERVER['HTTP_HOST'] && strpos($_SERVER['HTTP_HOST'], 'knopka.agency') !== false) { ?>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-188479547-1">
+        </script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-188479547-1');
+        </script>
+    <? } ?>
 </head>
 <body class="t-body <?= ($menuItemAlias=='home'? 'landing-page' : 'component-page') ?>" style="margin:0;"><!--allrecords-->
 <div id="allrecords" class="t-records" data-hook="blocks-collection-content-node" data-tilda-project-id="2512305"
