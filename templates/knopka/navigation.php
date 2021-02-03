8ecdf305fde5a3d0e90c30abf5f21f312a231227
Klyuchko Andrey <andreyalek@gmail.com>
2021-02-03 19:12:26 +0200

    Updated menu items and their style

---
+++ templates/knopka/navigation.php
@@ -0,0 +1,134 @@
+<div id="nav262006571" class="t446 t446__hidden t446__positionabsolute "
+     style="background-color: rgba(17,17,17,0.0); " data-bgcolor-hex="#111111"
+     data-bgcolor-rgba="rgba(17,17,17,0.0)" data-navmarker="nav262006571marker" data-appearoffset=""
+     data-bgopacity-two="" data-menushadow="" data-bgopacity="0.0" data-menu-items-align="center"
+     data-menu="yes">
+    <div class="t446__maincontainer t446__c12collumns" style="">
+        <div class="t446__leftwrapper" style="min-width:640px; padding-left:20px; padding-right:0px; text-align: left;">
+            <div class="t446__leftmenuwrapper">
+                <div style="display: block;margin-top:5px;margin-right:15px"><img
+                        src="https://static.tildacdn.com/tild3839-3161-4430-a335-636637303332/_.png"
+                        class="t446__imglogo t446__imglogomobile" imgfield="img"
+                        style="max-width: 70px; width: 70px;" alt=""></div>
+                <ul class="t446__list">
+                    <li class="t446__list_item"><a class="t-menu__link-item" href="/#about"
+                                                   data-menu-submenu-hook=""
+                                                   style="color:#43dde0;font-weight:600;"
+                                                   data-menu-item-number="1">О нас</a></li>
+
+                    <li class="t446__list_item"><a class="t-menu__link-item" href="/#service"
+                                                   data-menu-submenu-hook=""
+                                                   style="color:#43dde0;font-weight:600;"
+                                                   data-menu-item-number="2">Услуги</a></li>
+
+                    <li class="t446__list_item"><a class="t-menu__link-item" href="/projects"
+                                                   data-menu-submenu-hook=""
+                                                   style="color:#43dde0;font-weight:600;"
+                                                   data-menu-item-number="3">Проекты</a></li>
+
+                    <li class="t446__list_item"><a class="t-menu__link-item" href="/feedbacks"
+                                                   data-menu-submenu-hook=""
+                                                   style="color:#43dde0;font-weight:600;"
+                                                   data-menu-item-number="3">Отзывы</a></li>
+
+                    <li class="t446__list_item"><a class="t-menu__link-item" href="/prices"
+                                                   data-menu-submenu-hook=""
+                                                   style="color:#43dde0;font-weight:600;"
+                                                   data-menu-item-number="3">Цены</a></li>
+
+                    <li class="t446__list_item"><a class="t-menu__link-item" href="/#contact"
+                                                   data-menu-submenu-hook=""
+                                                   style="color:#43dde0;font-weight:600;"
+                                                   data-menu-item-number="3">Контакты</a></li>
+
+                </ul>
+            </div>
+        </div>
+        <div class="t446__rightwrapper" style="padding-right:20px; padding-left:85px; text-align: right;">
+            <div class="t446__rightmenuwrapper">
+                <ul class="t446__list"></ul>
+            </div>
+            <div class="t446__additionalwrapper">
+                <div class="t446__right_descr" style="color:#43dde0;font-size:20px;">Телефон: +380505080852
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>@media screen and (max-width: 980px) {
+        #rec262006571 .t446__leftcontainer {
+            padding: 20px;
+        }
+
+        #rec262006571 .t446__imglogo {
+            padding: 20px 0;
+        }
+
+        #rec262006571 .t446__mobile_burger span {
+            background-color: #43dde0;
+        }
+
+        #rec262006571 .t446__mobile_burger {
+            left: 20px;
+            right: unset;
+        }
+
+        #rec262006571 .t446__mobile_text {
+            text-align: right;
+            right: 20px;
+            left: unset;
+        }
+
+        @media screen and (max-width: 980px) {
+            #rec262006571 .t446 {
+                position: absolute;
+                top: 64px;
+            }
+        }
+    }</style>
+<script>    var t446__doResize;
+  $(window).resize(function () {
+    t446_checkOverflow('262006571', '0');
+    clearTimeout(t446__doResize);
+    t446__doResize = setTimeout(function () {
+      t446_checkOverflow('262006571', '0');
+    }, 200);
+  });
+  $(window).load(function () {
+    t446_checkOverflow('262006571', '0');
+  });
+  $(document).ready(function () {
+    t446_checkOverflow('262006571', '0');
+  });
+  $(document).ready(function () {
+    t446_highlight();
+  });
+  $(window).resize(function () {
+    t446_setBg('262006571');
+  });
+  $(document).ready(function () {
+    t446_setBg('262006571');
+  });</script>
+<script> $(document).ready(function () {
+    t446_createMobileMenu('262006571');
+  }); </script>
+<script type="text/javascript"> $(document).ready(function () {
+    setTimeout(function () {
+      t_onFuncLoad('t_menusub_init', function () {
+        t_menusub_init('262006571');
+      });
+    }, 500);
+  });</script>
+<style>@media screen and (max-width: 980px) {
+        #rec262006571 .t-menusub__menu .t-menusub__link-item {
+            color: #43dde0 !important;
+        }
+
+        #rec262006571 .t-menusub__menu .t-menusub__link-item.t-active {
+            color: #43dde0 !important;
+        }
+    }</style>
+<!--[if IE 8]>
+<style>#rec262006571 .t446 {
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorStr='#D9111111', endColorstr='#D9111111');
+}</style><![endif]--> </div>
