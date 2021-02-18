bc786b2431cebc5b6d7a6527265e153f1f35fec5
Klyuchko Andrey <andreyalek@gmail.com>
2021-02-18 13:32:46 +0200

    Added fb pixel

--- templates/knopka/index.php
+++ templates/knopka/index.php
@@ -122,6 +122,25 @@ JHtml::_('bootstrap.loadCss', false, $this->direction);
         <!-- Google AdSense -->
         <script data-ad-client="ca-pub-5639960772602363" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
+
+        <!-- Facebook Pixel Code -->
+        <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '881371576016688');
+          fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                       src="https://www.facebook.com/tr?id=881371576016688&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Facebook Pixel Code -->
+
     <? } ?>
 
 </head>
