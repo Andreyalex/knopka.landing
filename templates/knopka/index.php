811d7ee183e81f69bc411803e768ffb58cb19c03
andrii <andreyalek@gmail.com>
2022-03-28 21:01:34 +0300

    Set 0.10.0

--- templates/knopka/index.php
+++ templates/knopka/index.php
@@ -27,7 +27,7 @@ $config = JFactory::getConfig();
 $doc = $app->getDocument();
 $doc->setMetaData('author', 'Knopka.agency');
 
-$ver = '0.9.10';
+$ver = '0.10.0';
 
 JHtml::_('stylesheet', 'template.min.css', array('version' => $ver, 'relative' => true));
 JHtml::_('stylesheet', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/css/splide.min.css', array('relative' => false));
