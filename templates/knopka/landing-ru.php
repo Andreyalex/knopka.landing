33187670492f8633191a70d34000c357d44b2204
andrii <andreyalek@gmail.com>
2022-07-24 20:37:04 +0300

    Fixed slider settings

--- templates/knopka/landing-ru.php
+++ templates/knopka/landing-ru.php
@@ -568,11 +568,12 @@
     splider || (splider = new Splide('.splide', {
       type: 'loop',
       perPage: 3,
+      perMove: 1,
       pagination: false,
       arrows: false,
       autoplay: true,
-      interval: 1,
-      speed: 10000,
+      interval: 3000,
+      speed: 3000,
       easing: 'linear',
       gap: '3%'
     }));
